5eaa3a82a70d8728f8e87353a64acbc07b6f75c2
AleksandrKoltsov <41367097+AleksandrKoltsov@users.noreply.github.com>
2020-03-13 17:13:33 +0200

    jokes

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import './App.css';
+
+function RenderJoke (props) {
+  console.log(props);
+  return (
+      <div>
+        {props.jokes.map(el=><li key={el.id}>{el.joke}</li>)}
+      </div>
+  )
+}
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      jokes : []
+    }
+  }
+
+  getJoke(){
+    const link = 'http://api.icndb.com/jokes/random';
+    fetch(link).then(data => data.json()).then(d => {
+      const jokes = this.state.jokes.slice();
+      jokes.unshift(d.value);
+      this.setState({ jokes });
+    });
+  }
+  render(){
+    return (
+        <div className='container'>
+          <div>
+            <button className='gen' onClick={()=>this.getJoke()}>Next joke</button>
+          </div>
+          <ul>
+            <RenderJoke jokes={this.state.jokes}/>
+          </ul>
+        </div>
+    );
+  }
+}
+
+export default App;
+
