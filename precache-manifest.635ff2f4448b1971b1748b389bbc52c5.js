97d211cd070316c68843e1320117428e1c46c7b5
AleksandrKoltsov <41367097+AleksandrKoltsov@users.noreply.github.com>
2020-03-13 17:12:31 +0200

    Updates

---
+++ precache-manifest.635ff2f4448b1971b1748b389bbc52c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bec5f00f82c591aa27243254d7bbd5e",
+    "url": "/chukjokes/index.html"
+  },
+  {
+    "revision": "b26a5930fdcb328e5ad3",
+    "url": "/chukjokes/static/css/main.0fb47f0e.chunk.css"
+  },
+  {
+    "revision": "83809d7e4821035cee39",
+    "url": "/chukjokes/static/js/2.28568f04.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/chukjokes/static/js/2.28568f04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b26a5930fdcb328e5ad3",
+    "url": "/chukjokes/static/js/main.47f0a7e5.chunk.js"
+  },
+  {
+    "revision": "bf1b94e5b78c9b22f207",
+    "url": "/chukjokes/static/js/runtime-main.336cdebe.js"
+  }
+]);
